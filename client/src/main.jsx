7c2da20a27f7eec8d80237f8ce3c2566425f996c
aryaan-codes <2006aryankumar444@gmail.com>
2025-11-30 14:51:14 +0530

    Connected frontend to live backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom' // <--- Ye import karo
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://portfolio-api-otxm.onrender.com'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
